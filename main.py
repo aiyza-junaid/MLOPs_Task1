ebcdfadd23fb3707811cb0d4f36fd4dfa38fd98f
aiyza-junaid <aiyza.junaid@gmail.com>
2025-02-18 14:24:27 +0500

    committ

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deploying Lab Task at Vercel"
+    return "Deploying Lab Task aIYZA at Vercel"
 
 if __name__ == "__main__":
     import os
