3491531e665aed753d3fccec83832de06b44019d
aiyza-junaid <aiyza.junaid@gmail.com>
2025-02-18 14:24:13 +0500

    eee

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Deploying Lab Task at Vercel"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Vercel's PORT environment variable
+    app.run(host="0.0.0.0", port=port)
